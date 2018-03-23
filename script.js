55d783cf6af40884995f5d1fa04b799568c7c3ef
Patrycja Kania <el.czupakabra21@gmail.com>
2018-03-23 10:25:04 +0100

    Add files and content

---
+++ script.js
@@ -0,0 +1,23 @@
+var span = $("span");
+
+$("span:even").css('color','pink');
+
+var paragraphs = $('p');
+paragraphs.each(function(index, element) {
+
+    var button = '<button class="btn" data-tmp="' + index + '">Click me</button>'
+    $(element).append(button)
+
+});
+
+$("button").click(function(){
+	alert($(this).attr("data-tmp"));
+});
+
+$("button:odd").css('background-color','pink');
+$("button:even").css({
+    'background-color':'gray',
+    'color':'white',
+});
+
+$("button").css('margin-left', '100px');
